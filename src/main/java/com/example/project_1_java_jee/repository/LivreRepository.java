816d64fe256652a7f261d10da3b3a7a7b1838c4f
unknown <theplugofheishi412>
2026-07-30 02:28:04 +0200

    Livre Repository

--- src/main/java/com/example/project_1_java_jee/repository/LivreRepository.java
+++ src/main/java/com/example/project_1_java_jee/repository/LivreRepository.java
@@ -1,4 +1,52 @@
 package com.example.project_1_java_jee.repository;
 
+import com.example.project_1_java_jee.entity.Livre;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import java.util.*;
+
+@Stateless
 public class LivreRepository {
+
+    // injection de dependance de l'entityManager
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Livre livre){
+        entityManager.persist(livre);
+    }
+
+    public Livre findByAll(Long id){
+        return entityManager.find(Livre.class,id);
+    }
+
+
+    public List<Livre> findAll() {
+
+        return entityManager
+                .createQuery(
+                        "SELECT l FROM Livre l",
+                        Livre.class
+                )
+                .getResultList();
+    }
+
+    @Transactional
+    public void update(Livre livre){
+        entityManager.merge(livre);
+    }
+
+    @Transactional
+    public void delete(Livre livre) {
+        Livre livredelete =
+                entityManager.contains(livre)
+                        ? livre
+                        : entityManager.merge(livre);
+        entityManager.remove(livredelete);
+    }
+
 }
